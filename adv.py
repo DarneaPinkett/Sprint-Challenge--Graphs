62ed8ddcc94e2368d39e4552a62f8e5798d1ff65
Darnea A. Pinkett <msdarneapinkett@gmail.com>
2020-08-29 13:18:05 -0400

    Sprint

--- adv.py
+++ adv.py
@@ -5,6 +5,8 @@ from world import World
 import random
 from ast import literal_eval
 
+from util import Stack
+
 # Load world
 world = World()
 
@@ -34,7 +36,27 @@ traversal_path = []
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
 player.current_room = world.starting_room
-visited_rooms.add(player.current_room)
+
+moves = Stack()
+
+while len(visited_rooms) < len(world.rooms):
+    exits = player.current_room.get_exits()
+    possible_directions = []
+
+    for exit in exits:
+        if (exit is not None) and (player.current_room.get_room_in_direction(exit) not in visited_rooms):
+            possible_directions.append(exit)
+            
+    visited_rooms.add(player.current_room)
+
+    if len(possible_directions) > 0:
+        random_direction = random.randint(0, len(possible_directions) -1)
+        moves.push(possible_directions[random_direction])
+        player.travel(possible_directions[random_direction])
+        traversal_path.append(possible_directions[random_direction])
+    else:
+        last_move = moves.pop()
+
 
 for move in traversal_path:
     player.travel(move)
